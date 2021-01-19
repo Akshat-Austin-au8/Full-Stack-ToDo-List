c19241d157a7775b198152a4f8c06fb6a975a008
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2021-01-19 15:31:28 +0530

    connecting to the database

--- server/index.js
+++ server/index.js
@@ -3,3 +3,17 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://createproject:createproject123@cluster0.p8a8e.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${port}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
