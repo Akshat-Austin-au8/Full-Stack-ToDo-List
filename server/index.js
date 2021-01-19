570870f6ec1f3ea9c306137886eb564956c4bb56
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2021-01-19 21:54:51 +0530

    controller added

--- server/index.js
+++ server/index.js
@@ -1,23 +1,27 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import mongoose from 'mongoose';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
 
-import postRoutes from './routes/posts.js';
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use('/posts', postRoutes);
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://createproject:createproject123@cluster0.p8a8e.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const CONNECTION_URL =
+  "mongodb+srv://createproject:Akshat22081996@cluster0.p8a8e.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${port}`)))
-    .catch((error) => console.log(error.message));
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
 
-mongoose.set('useFindAndModify', false);
+mongoose.set("useFindAndModify", false);
