32fac75feebec6c248da618eff491a02d6e3ec12
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2021-01-20 23:58:40 +0530

    delete added

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -39,3 +39,14 @@ export const updatePost = async (req, res) => {
 
   res.json(updatePost);
 };
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("No post with that id");
+
+  const deletePost = await PostMessage.findByIdAndRemove(id);
+
+  res.json({ message: "Post deleted successfully" });
+};
