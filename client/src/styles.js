879f21e25d1dfadd20fcb2c66bf138b4c71dc014
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2021-01-21 01:13:41 +0530

    completed

--- client/src/styles.js
+++ client/src/styles.js
@@ -1,18 +1,18 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
   appBar: {
-    borderRadius: 5,
-    margin: "30px 0",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
+    borderRadius: 15,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   heading: {
-    color: "rgb(74, 109, 204)",
+    color: 'rgba(0,183,255, 1)',
   },
   image: {
-    marginLeft: "15px",
+    marginLeft: '15px',
   },
 }));
